1b7a6e1b30723ecdb68277df026f76391fd3cc82
Gianest <Kiki>
2019-03-24 22:00:45 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihanclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mahasiswa saya = new mahasiswa();
+            mahasiswa kamu = new mahasiswa();
+
+            saya.Nim = "12345";
+            saya.Nama = "joko";
+            saya.Ipk = "4";
+
+            kamu.Nim = "12346";
+            kamu.Nama = "jono";
+            kamu.Ipk = "3";
+
+            saya.registrasi();
+            saya.IsiKrs();
+
+            kamu.registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ mahasiswa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihanclass
+{
+    class mahasiswa
+    {
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public string Ipk { get; set; }
+
+        public void registrasi()
+        {
+            Console.WriteLine("Nama:{0}", Nama);
+            Console.WriteLine("NIM:{0}", Nim);
+            Console.WriteLine("IPK:{0}", Ipk);
+            Console.WriteLine("Telah melakukan registrasi");
+
+        }
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0}sedang melakukan proses Krs\n", Nama);
+        }
+    }
+}
